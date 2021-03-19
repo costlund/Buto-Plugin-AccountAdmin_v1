1957549b105926c8223e2883cbe273260f4d439d
Claes Östlund <claes@stenbergit.se>
2021-03-19 10:21:57 +0100

    1.9.2

--- js/function.js
+++ js/function.js
@@ -1,6 +1,6 @@
 function PluginAccountAdmin_v1(){
   this.account_view = function(id){
-    PluginWfBootstrapjs.modal({id: 'account_view', url: '/'+app.class+'/account_view/id/'+id, lable: 'Account', size: 'lg'});
+    PluginWfBootstrapjs.modal({id: 'account_view', url: '/'+app.class+'/account_view/id/'+id, lable: 'Account', size: 'xl'});
   }
   this.account_delete = function(){
     $.get('/'+app.class+'/account_delete_yes/id/'+app.account_id, function(){ 
